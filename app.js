24542eac79186433eddb4fb77b1e78a3a001d56f
robertfodor-job <79786361+robertfodor-job@users.noreply.github.com>
2022-02-04 20:34:43 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// routers
+
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...🧨`));
