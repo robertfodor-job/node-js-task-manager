53bf2be193e69861881904865dd5a8cc8317f217
robertfodor-job <79786361+robertfodor-job@users.noreply.github.com>
2022-02-05 09:41:35 +0100

    added basic logic

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
-
 const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+
+app.use(express.json());
 
 // routers
 
 app.get('/hello', (req, res) => {
   res.send('Task Manager App');
 });
+
+app.use('/api/v1/tasks', tasks);
+
+// app.get('/api/v1/tasks')         - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get a single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.detele('/api/v1/tasks/:id')  - delete task
 
 const port = 3000;
 
